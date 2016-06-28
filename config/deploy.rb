1cbbb6fffd7116a8841a3069c5f59acdcb717bb2
Ben Anderson <ben.anderson@powershop.co.nz>
2016-06-28 23:25:18 +1200

    Make rake executable, duh, fix deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.5.0'
 
 set :application, 'pxljam'
 set :repo_url, 'git@github.com:bluefantail/pxljam.git'
@@ -14,13 +14,13 @@ set :tmp_dir, "/tmp/#{`whoami`.split('\\').first.chomp}"
 set :deploy_to, '/apps/pxljam'
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'db/production.sqlite3', 'db/test.sqlite3')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 set :slack_webhook, 'https://hooks.slack.com/services/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'
 set :slack_team, "golden-eagle"
-set :slack_channel, '#pxljam'
+set :slack_channel, '#pxljm'
 
 after 'deploy:publishing', 'deploy:restart'
