375f9a6dae54481c9c50fc85c7882001bf1e1a4d
Matt <mattfannin@acidic.co.nz>
2016-07-03 21:42:56 +1200

    Fixed navigation active class updating.

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -183,18 +183,17 @@ if ('createTouch' in document) {
   } catch (e) {}
 }
 
- // Initialise Smooth Scroll and Gumshoe for Navigation
-  smoothScroll.init({
-    speed: 560, // Integer. How fast to complete the scroll in milliseconds
-    easing: 'easeInOutCubic', // Easing pattern to use
-    // offset: 1, // Integer. How far to offset the scrolling anchor location in pixels
-    updateURL: true, // Boolean. If true, update the URL hash on scroll
-  });
-  gumshoe.init({
-    // offset: 1, // Integer. How far to offset the scrolling anchor location in pixels
-  });
+// Initialise Smooth Scroll and Gumshoe for Navigation
+smoothScroll.init({
+  speed: 560, // Integer. How fast to complete the scroll in milliseconds
+  easing: 'easeInOutCubic', // Easing pattern to use
+  updateURL: true, // Boolean. If true, update the URL hash on scroll
+});
+gumshoe.init({
+  offset: 1 // Integer. How far to offset the scrolling anchor location in pixels
+});
 
-  gumshoe.setDistances();
+gumshoe.setDistances();
 // Logs
 console.log('Forms');
 console.log('-------------------------------------------------');
