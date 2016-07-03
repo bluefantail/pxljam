13ea206d09a23d36e79520b86d70e611678d7b07
Ben Anderson <ben.anderson@powershop.co.nz>
2016-07-04 11:21:51 +1200

    Some fixes to deployments

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'sodalite.acidic.co.nz', roles: %w{app db web}
+server 'sodalite.acidic.co.nz', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
