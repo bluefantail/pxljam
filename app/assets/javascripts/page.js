c9f2dabfb9648e22d04fd5266adc9ba0ef68fd9d
Matt <mattfannin@acidic.co.nz>
2016-07-12 00:34:06 +1200

    Work to integrate old graphics and animations.

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -190,10 +190,98 @@ smoothScroll.init({
   updateURL: true, // Boolean. If true, update the URL hash on scroll
 });
 gumshoe.init({
-  offset: 1 // Integer. How far to offset the scrolling anchor location in pixels
+  offset: 3 // Integer. How far to offset the scrolling anchor location in pixels
 });
 
-gumshoe.setDistances();
+/////////////////////////////////////
+// Old Artwork Insertion (jquery) //
+///////////////////////////////////
+$(function() {
+  const conts = $(".container");
+  const body  = $("body");
+
+  $(window).resize(function() {
+    conts.width(body.outerWidth());
+  }).resize();
+
+  function grid(width, height, variation, xGap, yGap, chance, cont, make) {
+    const els = [];
+
+    var x, y, left, top, el;
+
+    for (x = -xGap; x < width + xGap; x += xGap * Math.random() + xGap) {
+      for (y = variation; y < height; y += yGap * Math.random() + yGap) {
+        if (Math.random() < chance) {
+          el = make;
+
+          left = parseInt(x + Math.random() * variation, 10);
+          top  = parseInt(y + Math.random() * variation, 10);
+
+          el.css({ left: left, top: top });
+          cont.append(el);
+
+          els.push(el);
+        }
+      }
+    }
+
+    return els;
+  }
+
+  $('.space').load(function() {
+    const space  = $('.space');
+    const cont   = $(".stars");
+
+    console.log(space);
+
+    const square = 20;
+    const gap    = 40;
+    const chance = 0.2;
+
+    const width  = space.outerWidth() - square;
+    const height = space.outerHeight() - square;
+
+    cont.height(space.outerHeight());
+
+    grid(width, height, square, gap, gap, chance, cont, function() {
+      return $('<div class="star"></div>');
+    });
+  });
+
+  (function() {
+    const sky     = $("header");
+    // const cont    = $(".clouds");
+
+    const square  = 50;
+    const xGap    = 200;
+    const yGap    = 100;
+    const chance  = 0.4;
+
+    const width   = sky.outerWidth() - square;
+    const height  = sky.outerHeight() - yGap;
+
+    // cont.height(sky.outerHeight());
+
+    const clouds = grid(width, height, square, xGap, yGap, chance,
+      function() {
+        return $('<div class="cloud"></div>');
+      });
+
+    $.each(clouds, function() {
+      const cloud = $(this);
+      animate();
+
+      function animate() {
+        setTimeout(function() {
+          const moveBy = Math.random() > 0.5 ? 1 : -1;
+          cloud.css('left', cloud.position().left + moveBy + 'px');
+          animate();
+        }, Math.random() * 6000 + 4000);
+      }
+    });
+  })();
+});
+
 // Logs
 console.log('Forms');
 console.log('-------------------------------------------------');
