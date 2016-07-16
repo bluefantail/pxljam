46a336721cc76e09a0e659dc3a93387c6a93fc09
Matt Fannin <mattfannin@acidic.co.nz>
2016-07-16 15:30:45 +1200

    Clouds rendering.

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -197,12 +197,12 @@ gumshoe.init({
 // Old Artwork Insertion (jquery) //
 ///////////////////////////////////
 $(function() {
-  const conts = $(".container");
+  const conts = $(".art-container");
   const body  = $("body");
 
-  $(window).resize(function() {
-    conts.width(body.outerWidth());
-  }).resize();
+  // $(window).resize(function() {
+    // conts.width(body.outerWidth());
+  // }).resize();
 
   function grid(width, height, variation, xGap, yGap, chance, cont, make) {
     const els = [];
@@ -212,7 +212,7 @@ $(function() {
     for (x = -xGap; x < width + xGap; x += xGap * Math.random() + xGap) {
       for (y = variation; y < height; y += yGap * Math.random() + yGap) {
         if (Math.random() < chance) {
-          el = make;
+          el = make();
 
           left = parseInt(x + Math.random() * variation, 10);
           top  = parseInt(y + Math.random() * variation, 10);
@@ -228,11 +228,9 @@ $(function() {
     return els;
   }
 
-  $('.space').load(function() {
-    const space  = $('.space');
-    const cont   = $(".stars");
-
-    console.log(space);
+  (function() {
+    const space  = $('#home');
+    const cont   = $("#stars");
 
     const square = 20;
     const gap    = 40;
@@ -249,20 +247,17 @@ $(function() {
   });
 
   (function() {
-    const sky     = $("header");
-    // const cont    = $(".clouds");
+    const sky    = $("#clouds"); // #upper-sky
 
-    const square  = 50;
-    const xGap    = 200;
-    const yGap    = 100;
-    const chance  = 0.4;
+    const square = 10;
+    const xGap   = 220;
+    const yGap   = 100;
+    const chance = 0.25;
 
-    const width   = sky.outerWidth() - square;
-    const height  = sky.outerHeight() - yGap;
+    const width  = sky.outerWidth() - square;
+    const height = sky.outerHeight() - yGap;
 
-    // cont.height(sky.outerHeight());
-
-    const clouds = grid(width, height, square, xGap, yGap, chance,
+    const clouds = grid(width, height, square, xGap, yGap, chance, sky,
       function() {
         return $('<div class="cloud"></div>');
       });
@@ -273,19 +268,149 @@ $(function() {
 
       function animate() {
         setTimeout(function() {
-          const moveBy = Math.random() > 0.5 ? 1 : -1;
+          const moveBy = Math.random() > 0.75 ? 1 : -2;
           cloud.css('left', cloud.position().left + moveBy + 'px');
           animate();
-        }, Math.random() * 6000 + 4000);
+        }, Math.random() * 2000 + 1000);
       }
     });
   })();
+
+  (function() {
+    const sky   = $("#home"); // #sky
+    const fly   = $("#fly");
+    const kite  = $("#kite");
+    const line  = $("#line");
+    const flyer = $("#flyer");
+
+    const kiteOffsetX = 24;
+    const kiteOffsetY = 64;
+
+    var flyerOffsetX = 60;
+    var flyerOffsetY = 40;
+    var reeled = false;
+
+    var loaded = false;
+
+    function newLinePixel() {
+      const pixel = $('<div class="kite-line"></div>');
+      line.append(pixel);
+      return pixel;
+    }
+
+    $("#fly img").load(function() {
+      if (!loaded) {
+        return loaded = true;
+      }
+
+      var width = sky.width() / 8 + Math.random() * 50 - 25;
+      width -= width % 4;
+      var height = sky.height() - kite.height() - Math.random() * 200 - 100;
+      height -= height % 4;
+
+      var flyerX = 50 + flyerOffsetX;
+      var kiteX  = width + flyerX + kiteOffsetX;
+      var kiteY  = height + kiteOffsetY;
+
+      function placeKite() {
+        kite.css('right', kiteX - kiteOffsetX - 2);
+        kite.css('bottom', kiteY - kiteOffsetY + 4);
+      }
+
+      placeKite();
+      flyer.css('right', flyerX - flyerOffsetX + 2);
+      flyer.css('bottom', 0);
+
+      const pixels = [];
+      const pixelCount = 1000;
+      for (var i = pixelCount - 1; i >= 0; i--) {
+        pixels.push(newLinePixel());
+      }
+
+      function alignPixels() {
+        const xdiff = (kiteX - flyerX) / pixelCount;
+        const ydiff = (kiteY - flyerOffsetY) / pixelCount;
+
+        for (var i = 0; i < pixelCount; i++) {
+          var pixel = pixels[i];
+
+          var x = kiteX - i * xdiff;
+          var y = kiteY - i * ydiff;
+          x -= x % 4;
+          y -= y % 4;
+
+          pixel.x = x;
+          pixel.y = y;
+          pixel.css('right', x);
+          pixel.css('bottom', y);
+        }
+
+        var hidden = false;
+        var prev, next;
+        for (var i = 0; i < pixelCount; i++) {
+          var pixel = pixels[i];
+
+          if (i < pixelCount - 1) {
+            var next = pixels[i + 1];
+
+            if (next.x == pixel.x && next.y == pixel.y) {
+              pixel.hide();
+              continue;
+            }
+
+            if (i > 0 && (pixel.x == prev.x || pixel.y == prev.y) &&
+                (pixel.x == next.x || pixel.y == next.y) &&
+                next.x != prev.x && next.y != prev.y) {
+              pixel.hide();
+              continue;
+            }
+
+            pixel.show();
+            prev = pixel;
+          }
+        }
+      }
+
+      alignPixels();
+
+      kite.show();
+      flyer.show();
+
+      setInterval(function() {
+        if (Math.random() < 0.2) {
+          if (reeled) {
+            flyerOffsetX += 4;
+            flyerOffsetY -= 4;
+            reeled = false;
+          } else {
+            flyerOffsetX -= 4;
+            flyerOffsetY += 4;
+            reeled = true;
+          }
+        }
+
+        alignPixels();
+      }, 300);
+
+      setInterval(function() {
+        if (Math.random() > 0.5) {
+          kiteX += Math.random() > 0.5 ? 4 : -4;
+        } else {
+          kiteY += Math.random() > 0.5 ? 4 : -4;
+        }
+
+        placeKite();
+        alignPixels();
+      }, 2000);
+    });
+  })();
 });
+
+
 
 // Logs
 console.log('Forms');
 console.log('-------------------------------------------------');
 console.log('Players Object:');
-console.table(gumshoe);
 console.log(playerElements);
 console.log('-------------------------------------------------');
